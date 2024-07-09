9e35c8bd7f287b2c79403db6a6a0c1118e5f2467
hamilton <hamilton-souza@outlook.com.br>
2024-07-09 19:38:24 -0300

    initial commit

---
+++ src/main/java/br/edu/ifsul/cstsi/aulatads/AulaTadsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.aulatads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaTadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AulaTadsApplication.class, args);
+    }
+
+}
